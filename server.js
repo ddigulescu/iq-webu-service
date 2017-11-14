379cbd0f3762af51b064f1d388a8bd29110f32c6
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2017-11-14 16:07:19 +0200

    Add students and add student endpoints

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const { Student } = require("./app/models");
 const app = express();
 const port = process.env.PORT || 8080;
 
+const errHandler = res => err => res.json({ error: "Something went wrong" });
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,10 +21,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.json({ message: "Hello" });
 });
+app.get("/students", (req, res) => {
+  Student.find()
+    .then(students => (students.length > 0 ? res.json(students) : res.json([])))
+    .catch(errHandler(res));
+});
+app.post("/students/add", (req, res) => {
+  const { name, email } = req.body;
+  const student = new Student({
+    name,
+    email,
+    postAdded: true
+  });
+  return student
+    .save()
+    .then(doc => res.json(doc))
+    .catch(errHandler(res));
+});
 app.get("/byemail", (req, res) => {
-  Student.findOne({ email: req.query.email }).then(student =>
-    res.json(student)
-  );
+  Student.findOne({ email: req.query.email })
+    .then(student => res.json(student))
+    .catch(errHandler(res));
 });
 app.post("/greet", (req, res) => {
   const { name, email } = req.body;
@@ -32,11 +51,9 @@ app.post("/greet", (req, res) => {
       return student.save();
     })
     .then(student => {
-      res.json({ name: student.name });
+      return res.json({ name: student.name });
     })
-    .catch(() => {
-      res.json({ error: "Something went wrong" });
-    });
+    .catch(errHandler(res));
 });
 app.use((req, res) => res.json({ error: "Not found" }));
 
